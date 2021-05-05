c002c9aae8030928eee05b461d8220d3a9da9340
incrazyboyy <c.m.f@online.de>
2021-05-05 23:28:43 +0200

    Added dummy project

---
+++ steiner_flierler_cpp/dummy/dummy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dummy
+//
+//  Created by Chris on 05.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Marc Kurz!" << std::endl;
+    return 0;
+}
