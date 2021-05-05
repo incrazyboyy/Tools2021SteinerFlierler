3622c10207f11230b0cadb4ce1af1d0b4d2386a4
steinmar2 <mariosteiner1997@gmail.com>
2021-05-05 23:40:08 +0200

    Added some fancy code in main.cpp

--- steiner_flierler_cpp/dummy/dummy/main.cpp
+++ steiner_flierler_cpp/dummy/dummy/main.cpp
@@ -10,5 +10,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, Marc Kurz!" << std::endl;
+    if(argc) {
+        std::cout << "argc is always atleast 1" << std::endl;
+    }
     return 0;
 }
